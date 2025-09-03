11773e72831487c88a7522466bcb8f9894855ff9
Tan Jia Huei <tanjiahuei@gmail.com>
2025-09-03 10:36:03 +0000

    update

--- basic/setup.py
+++ basic/setup.py
@@ -3,13 +3,10 @@ import glob
 from Cython.Build import cythonize
 from setuptools import find_packages, setup
 
-# Find all Python files in the octo package
-python_files = glob.glob("src/**/*.py")
-
 setup(
     name="octo",
     version="1.0.0",
     packages=find_packages(),
-    ext_modules=cythonize(python_files),
+    ext_modules=cythonize(glob.glob("src/**/*.py", recursive=True)),
     zip_safe=False,
 )
